8b72f761be551c067ef3070b5970102a24a3d5f3
Raulha9 <Raulajm07@gmail.com>
2023-11-05 12:10:10 +0100

    Gestión de Pedidos Finalizado hecho por Raúl Herrera Alba 2ºDAM

---
+++ src/main/java/com/example/gestiondepedidos/controllers/VentanaPedidoController.java
@@ -0,0 +1,88 @@
+package com.example.gestiondepedidos.controllers;
+
+import com.example.gestiondepedidos.Main;
+import com.example.gestiondepedidos.Sesion;
+import com.example.gestiondepedidos.domain.DBConnection;
+import com.example.gestiondepedidos.item.Item;
+import com.example.gestiondepedidos.item.ItemDAOImp;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controlador de la ventana de detalles de pedido en la aplicación de gestión de pedidos.
+ * Esta clase gestiona la interfaz de usuario para mostrar los detalles de un pedido y permite al usuario cerrar sesión o volver atrás.
+ */
+public class VentanaPedidoController implements Initializable {
+    @javafx.fxml.FXML
+    private Button btnCerrarSesion;
+    @javafx.fxml.FXML
+    private TableView tvPedido;
+    @javafx.fxml.FXML
+    private TableColumn<Item, String> colId;
+    @javafx.fxml.FXML
+    private TableColumn<Item, String> colCodPedido;
+    @javafx.fxml.FXML
+    private TableColumn<Item, String> colCantidad;
+    @javafx.fxml.FXML
+    private TableColumn<Item, String> colProducto;
+    @javafx.fxml.FXML
+    private Button btnVolver;
+
+    private ObservableList<Item> observableListItem;
+
+    /**
+     * Constructor de la clase VentanaPedidoController.
+     */
+    public VentanaPedidoController(){}
+
+    /**
+     * Inicializa la interfaz de usuario y carga los detalles del pedido seleccionado.
+     *
+     * @param url             Ubicación relativa del archivo FXML.
+     * @param resourceBundle  Recursos utilizados para la inicialización.
+     */
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        this.colId.setCellValueFactory((fila) ->{
+            String id = String.valueOf(fila.getValue().getId());
+            return new SimpleStringProperty(id);
+        });
+        this.colCodPedido.setCellValueFactory((fila) -> {
+            String cPedido = fila.getValue().getCodigo_pedido();
+            return new SimpleStringProperty(cPedido);
+        });
+        this.colCantidad.setCellValueFactory((fila) -> {
+            String cCantidad = String.valueOf(fila.getValue().getCantidad());
+            return new SimpleStringProperty(cCantidad);
+        });
+        this.colProducto.setCellValueFactory((fila) -> {
+            String cProducto = String.valueOf(fila.getValue().getProducto_id().getNombre());
+            return new SimpleStringProperty(cProducto);
+        });
+
+        this.observableListItem = FXCollections.observableArrayList();
+        ItemDAOImp itemDAOImp = new ItemDAOImp(DBConnection.getConnection());
+        Sesion.setItems(itemDAOImp.loadAll(Sesion.getPedido().getCodigo()));
+        this.observableListItem.addAll(Sesion.getItems());
+        this.tvPedido.setItems(this.observableListItem);
+    }
+
+    @javafx.fxml.FXML
+    public void logout(ActionEvent actionEvent) {
+        Sesion.setUsuario(null);
+        Main.loadFXMLLogin("login.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void volver(ActionEvent actionEvent) {
+        Main.loadFXMLUsuario("ventana-usuario.fxml");
+    }
+}
---
+++ src/main/java/com/example/gestiondepedidos/controllers/VentanaUsuarioController.java
@@ -0,0 +1,104 @@
+package com.example.gestiondepedidos.controllers;
+
+import com.example.gestiondepedidos.Main;
+import com.example.gestiondepedidos.Sesion;
+import com.example.gestiondepedidos.domain.DBConnection;
+import com.example.gestiondepedidos.pedido.Pedido;
+import com.example.gestiondepedidos.pedido.PedidoDAOImp;
+import javafx.beans.Observable;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controlador de la ventana de usuario en la aplicación de gestión de pedidos.
+ * Esta clase gestiona la interfaz de usuario para mostrar los pedidos del usuario y permite al usuario cerrar sesión.
+ */
+public class VentanaUsuarioController implements Initializable {
+    @javafx.fxml.FXML
+    private Button btnCerrarSesion;
+    @javafx.fxml.FXML
+    private TableView<Pedido> tvPedidos;
+    @javafx.fxml.FXML
+    private TableColumn<Pedido, String> colIdPedido;
+    @javafx.fxml.FXML
+    private TableColumn<Pedido, String> colCodPedido;
+    @javafx.fxml.FXML
+    private TableColumn<Pedido, String> colFecha;
+    @javafx.fxml.FXML
+    private TableColumn<Pedido, String> colUsuario;
+    @javafx.fxml.FXML
+    private TableColumn<Pedido, String> colTotal;
+
+    private ObservableList<Pedido> pedidosObservable;
+
+    /**
+     * Constructor de la clase VentanaUsuarioController.
+     */
+    public VentanaUsuarioController(){}
+
+    /**
+     * Inicializa la interfaz de usuario y carga los pedidos del usuario actual.
+     *
+     * @param url             Ubicación relativa del archivo FXML.
+     * @param resourceBundle  Recursos utilizados para la inicialización.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.colIdPedido.setCellValueFactory((fila)->{
+            String id = String.valueOf((fila.getValue().getId()));
+            return new SimpleStringProperty(id);
+        });
+        this.colFecha.setCellValueFactory((fila)->{
+            String fecha = String.valueOf((fila.getValue().getFecha()));
+            return new SimpleStringProperty(fecha);
+        });
+        this.colCodPedido.setCellValueFactory((fila) -> {
+            String codigoPedido = fila.getValue().getCodigo();
+            return new SimpleStringProperty(codigoPedido);
+        });
+        this.colUsuario.setCellValueFactory((fila) -> {
+            String usuarioId = String.valueOf(fila.getValue().getUsuarioId());
+            return new SimpleStringProperty(usuarioId);
+        });
+        this.colTotal.setCellValueFactory((fila) -> {
+            String total = String.valueOf(fila.getValue().getTotal());
+            return new SimpleStringProperty(total);
+        });
+
+        this.pedidosObservable = FXCollections.observableArrayList();
+        PedidoDAOImp pedidoDAOImp = new PedidoDAOImp(DBConnection.getConnection());
+        Sesion.setPedidos(pedidoDAOImp.loadAll(Sesion.getUsuario().getId()));
+        this.pedidosObservable.addAll(Sesion.getPedidos());
+        this.tvPedidos.setItems(this.pedidosObservable);
+        this.tvPedidos.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->{
+            this.seleccionarPedido(this.tvPedidos.getSelectionModel().getSelectedItem());
+        });
+    }
+
+    /**
+     * Maneja la selección de un pedido en la tabla y carga los detalles del pedido seleccionado.
+     *
+     * @param pedido Pedido seleccionado.
+     */
+    private void seleccionarPedido(Pedido pedido) {
+        Sesion.setPedido(pedido);
+        Sesion.setPos(this.tvPedidos.getSelectionModel().getSelectedIndex());
+        Main.loadFXMLDetalles("ventana-pedido.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void logout(ActionEvent actionEvent) {
+        Sesion.setUsuario(null);
+        Main.loadFXMLLogin("login.fxml");
+    }
+}
